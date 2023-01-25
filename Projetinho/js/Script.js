ec6de2b8f364bff599f219f0a528f1d2fc03ffac
Hudyson Silva <95923534+hudysonSilva@users.noreply.github.com>
2023-01-25 11:35:45 -0300

    Jogo completo testado
    A animação das nuvens não param quando o jogo termina porem eu optei por deixar assim

---
+++ Projetinho/js/Script.js
@@ -0,0 +1,36 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () =>{
+
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+         mario.classList.remove('jump'); 
+    },700);
+}
+
+const loop = setInterval(()=>{
+
+    const pipePosition = pipe.offsetLeft;
+    const maroPosition = +window.getComputedStyle(mario).bottom.replace('px','')
+      
+    if (pipePosition <= 120 && pipePosition > 0 && maroPosition < 80){
+
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${maroPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+    }
+
+
+},10);
+
+document.addEventListener('keydown', jump);
